e8a6b6ca20f165c7df2d13f823ddb347629828c9
loaamiera <brownamiera@gmail.com>
2025-06-15 07:41:21 -0700

    Update Foundations.js

--- Foundations.js
+++ Foundations.js
@@ -1,4 +1,6 @@
-
+option = document.getElementById('Marx');
+placing = document.getElementById('placement');
+information = function () {
 			fetch('https://loaamiera.github.io/Locke/Foundations.JSON')
 			.then((response) => {    
           		if (!response.ok) {
@@ -8,23 +10,16 @@
         					})
         		.then((data) => {
 				for (const info of data.Foundation) {
-					option = document.getElementById('Marx');
-					placing = document.getElementById('placement');
           				optionMarx = info[1]
 			                option.onclick = function wording () {
-					              placing.innerHtml =
-			                    <h2>${optionMarx.title}</h2> 
-			                    <img src="${optionMarx.imageUrl}" alt="${optionMarx.altText}" style="width:100%;max-width:300px;">
-			               <!-- Displays an image with a source and alternative text from the item, with a set style -->
-			               <!-- Displays the altitude of the item -->
-			                    <p>${optionMarx.text}  <a href = "${optionMarx.Link}" target = "_blank">Read More</a></p>
-			               <!-- Displays text and a 'Read More' link that opens in a new tab -->
-			                ;
-			                // Appends the newly created section to the 'content' div.
-			                intro.appendChild('placing'); } ;
-			            });
-			        })
-			        .catch(error => console.error('Error loading the data:', error)); 
-			        // Catches and logs any errors in the fetch operation.
+					              const pMarx = document.createElement("p");
+						      const strongMarx = document.createElement("strong");
+            					      strongMarx.textContent = ${optionMarx.title};
+						      const imageMarx = '<img src="${optionMarx.imageUrl}" alt="${optionMarx.altText}">' ;
+						      const textMarx = ${optionMarx.text} ;
+					              const linkMarx = '<a href = "${optionMarx.Link}" target = "_blank">Click to read</a></p>'  ;
+					              option.appendChild('placing') ;
+			        					}}})
+			        .catch(error => console.error('Error loading the data:', error)) 
+					} ;
 		
-				});
