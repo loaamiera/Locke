3fec2b0664d68ed1ede3c2733eb26ab1c3d81466
loaamiera <brownamiera@gmail.com>
2025-06-15 15:10:43 -0700

    Update Foundations.js

--- Foundations.js
+++ Foundations.js
@@ -1,6 +1,7 @@
 const option = document.getElementById('Intro');
 const marxOption = document.getElementById('Marx');
 const placing = document.getElementById('placement');
+const myInfo = '' ;
 
 
 document.addEventListener('DOMContentLoaded', 
@@ -15,24 +16,25 @@ document.addEventListener('DOMContentLoaded',
 		 		marxOption.onclick = function marxInfo() {
 		 				var optionFinal = placing.innerhtml
 						var optionSelection = info.Marx ;
-		 				var optionTitle = optionSelection.title ;
-						var content = optionSelection.text ;
 						var strong = document.createElement("strong") ;
+						strong.textContent = optionSelection.title ;
 		 				var par = document.createElement("p") ;
-            					strong.textContent = info.title  ;
-		 				par.textContent = content ;
-						myInfo.append(strong, par) ;
-						myInfo.append('<img ${info.imageUrl} alt=${info.altText}> </img>') ;
-						myInfo.append('<a href = ${info.Link} target = "_blank"> Click to read. </a>');
-						optionFinal.innerText = myInfo ;
-			         option.onclick = function wording () {
-					              placing.append(myInfo)} ; 
+		 				par.textContent = optionSelection.text ;
+						var link = document.createElement("a") ;
+						link.href = optionSelection.Link ;
+						link.target = "_blank" ;
+						pic.alt = optionSelection.altText ;
+						var pic = document.createElement("img") ;
+						pic.src = optionSelection.imageUrl ;
+						pic.alt = optionSelection.altText ;
+						myInfo.append(strong, par, pic, link) ;
+						placing.innerText = myInfo ;
 
 				};}) 
 
 	.catch((error) => {
-				console.error(
-					placement.append(document.createTextNode('Error: ${error.message}'));
-						);
-			});		
-	) ;
+				const mistake = document.createTextNode(`Error: ${error.message}`);
+      				placing.appendChild(mistake);
+      				console.error(error);
+    });
+});
