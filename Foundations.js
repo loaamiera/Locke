a2d33bc890dfc6db475eb67ff1e7c963636950b0
loaamiera <brownamiera@gmail.com>
2025-06-15 15:20:29 -0700

    Update Foundations.js

--- Foundations.js
+++ Foundations.js
@@ -4,16 +4,16 @@ const placing = document.getElementById('placement');
 const myInfo = '' ;
 
 
-document.addEventListener('DOMContentLoaded', 
-	info = fetch('Foundations.JSON')
+document.addEventListener('DOMContentLoaded', () =>
+	{info = fetch('Foundations.JSON')
 	.then((response) => {    
           		if (!response.ok) {
             		throw new Error(`HTTP error, status = ${response.status}`);
-          		}
+          		} 
           		return response.json();
         					})
          .then((data) => {	
-		 		marxOption.onclick = function marxInfo() {
+		 		marxOption.onclick = function marxInfo () {
 		 				var optionFinal = placing.innerhtml
 						var optionSelection = info.Marx ;
 						var strong = document.createElement("strong") ;
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded',
 						var link = document.createElement("a") ;
 						link.href = optionSelection.Link ;
 						link.target = "_blank" ;
+						link.textContent = 'Click to read' ;
 						pic.alt = optionSelection.altText ;
 						var pic = document.createElement("img") ;
 						pic.src = optionSelection.imageUrl ;
