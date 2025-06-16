66654602043f48218461cba3c8ee056680c951e8
loaamiera <brownamiera@gmail.com>
2025-06-15 17:19:40 -0700

    Update Foundations.js

--- Foundations.js
+++ Foundations.js
@@ -1,5 +1,13 @@
 const option = document.getElementById('Intro');
 const marxOption = document.getElementById('Marx');
+const philosophyOption = document.getElementById("Who") ;
+const quotesOption = document.getElementById("Quotes") ;
+const hegelOption = document.getElementById("Hegel") ;
+const exampleOption = document.getElementById("Popular") ;
+const schopenhauerOption = document.getElementById("Schopenhauer") ;
+const haterOption = document.getElementById("Unlikeable") ;
+const oppositesOption = document.getElementById("Opposites") ;
+const politicsOption = document.getElementById("Politics") ;
 const placing = document.getElementById('placement');
 const myInfo = '' ;
 
@@ -15,6 +23,44 @@ document.addEventListener('DOMContentLoaded',
 		 		marxOption.onclick = function marxInfo () {
 		 				var optionFinal = placing.innerhtml
 						var optionSelection = info.Marx ;
+						var strong = document.createElement("strong") ;
+						strong.textContent = optionSelection.title ;
+		 				var par = document.createElement("p") ;
+		 				par.textContent = optionSelection.text ;
+						var link = document.createElement("a") ;
+						link.href = optionSelection.Link ;
+						link.target = "_blank" ;
+						link.textContent = 'Click to read' ;
+						pic.alt = optionSelection.altText ;
+						var pic = document.createElement("img") ;
+						pic.src = optionSelection.imageUrl ;
+						pic.alt = optionSelection.altText ;
+						myInfo.append(strong, par, pic, link) ;
+						placing.innerText = myInfo ;
+
+				};
+			  philosophyOption.onclick = function philInfo () {
+		 				var optionFinal = placing.innerhtml
+						var optionSelection = info.Who ;
+						var strong = document.createElement("strong") ;
+						strong.textContent = optionSelection.title ;
+		 				var par = document.createElement("p") ;
+		 				par.textContent = optionSelection.text ;
+						var link = document.createElement("a") ;
+						link.href = optionSelection.Link ;
+						link.target = "_blank" ;
+						link.textContent = 'Click to read' ;
+						pic.alt = optionSelection.altText ;
+						var pic = document.createElement("img") ;
+						pic.src = optionSelection.imageUrl ;
+						pic.alt = optionSelection.altText ;
+						myInfo.append(strong, par, pic, link) ;
+						placing.innerText = myInfo ;
+
+				};
+		 	quotesOption.onclick = function quoteInfo () {
+		 				var optionFinal = placing.innerhtml
+						var optionSelection = info.Quotes ;
 						var strong = document.createElement("strong") ;
 						strong.textContent = optionSelection.title ;
 		 				var par = document.createElement("p") ;
