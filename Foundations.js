503f8f629dfe6d7d2fb7d484e801280c1b4515de
loaamiera <brownamiera@gmail.com>
2025-06-15 22:05:27 -0700

    Update Foundations.js

--- Foundations.js
+++ Foundations.js
@@ -19,307 +19,296 @@ const upsetOption = document.getElementById("Upset") ;
 const placing = document.getElementById('placement');
 const myInfo = document.createElement('div') ;
 
-
-info = fetch('Foundations.JSON')
-	.then((response) => {    
-          		if (!response.ok) {
-            		throw new Error(`HTTP error, status = ${response.status}`);
-          		} 
-          		return response.json() ;})
-         .then((data) => {	
-		 		marxOption.onClick = function marxInfo () {
+over = async function () {
+info = await fetch('Foundations.JSON')
+} ;
+marxOption.onClick = function marxInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Marx ;				
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
-						alt.value = optionSelection.altText ;
-						src.value = optionSelection.imageUrl ;
+						alt.value = optionSelection.altText() ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-			  philosophyOption.onClick = function philInfo () {
+philosophyOption.onClick = function philInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Who ;
 						
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	quotesOption.onClick = function quoteInfo () {
+quotesOption.onClick = function quoteInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Quotes ;	
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
+						alt.value = optionSelection.altText() ;
 
-						src.value = optionSelection.imageUrl ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	hegelOption.onClick = function hegelInfo () {
+hegelOption.onClick = function hegelInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.hegel ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
+						alt.value = optionSelection.altText() ;
 
-						src.value = optionSelection.imageUrl ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	exampleOption.onClick = function exampleInfo () {
+exampleOption.onClick = function exampleInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Popular ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
-						src.value = optionSelection.imageUrl ;
+						alt.value = optionSelection.altText() ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	schopenhauerOption.onClick = function schopenhauerInfo () {
+schopenhauerOption.onClick = function schopenhauerInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Schopenhauer ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
-						src.value = optionSelection.imageUrl ;
+						alt.value = optionSelection.altText() ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
 		 	haterOption.onClick = function haterInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Unlikeable ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	oppositesOption.onClick = function oppositesInfo () {
+oppositesOption.onClick = function oppositesInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Opposites ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
-						src.value = optionSelection.imageUrl ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};	
-		 	politicsOption.onClick = function politicsInfo () {
+politicsOption.onClick = function politicsInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Politics ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	videoOption.onClick = function videoInfo () {
+videoOption.onClick = function videoInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Videos ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to watch' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	engelsOption.onClick = function engelsInfo () {
+engelsOption.onClick = function engelsInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Engels ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
+						alt.value = optionSelection.altText() ;
 
-						src.value = optionSelection.imageUrl ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	coverageOption.onClick = function coverageInfo () {
+coverageOption.onClick = function coverageInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Coverage ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	baudrillardOption.onClick = function baudrillardInfo () {
+baudrillardOption.onClick = function baudrillardInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Baudrillard ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
-						src.value = optionSelection.imageUrl ;
+						alt.value = optionSelection.altText() ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	elaborationOption.onClick = function elaborationInfo () {
+elaborationOption.onClick = function elaborationInfo () {
 		 				var optionFinal = placing.innerHTML
-						var optionSelection = info.Elaboration ;
-						
-						var Strong = optionSelection.title ;
+						var optionSelection = info.Elaboration	
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 				  		var pic = document.createElement("img") ;
 				  		var alt = pic.createElement('alt') ;
-						alt.value = optionSelection.altText ;
-						src.value = optionSelection.imageUrl ;
+						alt.value = optionSelection.altText() ;
+						src.value = optionSelection.imageUrl() ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	foundationOption.onClick = function foundationInfo () {
+foundationOption.onClick = function foundationInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Foundation ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	revolutionOption.onClick = function revolutionInfo () {
+revolutionOption.onClick = function revolutionInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Revolution ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	crisisOption.onClick = function crisisInfo () {
+crisisOption.onClick = function crisisInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Crisis ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-		 	upsetOption.onClick = function crisisInfo () {
+upsetOption.onClick = function crisisInfo () {
 		 				var optionFinal = placing.innerHTML
 						var optionSelection = info.Crisis ;
-						var Strong = optionSelection.title ;
+						var Strong = optionSelection.title() ;
 		 				var par = optionSelection.text ;
 						var link = document.createElement("a") ;
-						href.value = optionSelection.Link ;
+						href.value = optionSelection.Link() ;
 				  		var target = link.createAttribute('target') ;
 						target.value = "_blank" ;
 						link.textContent = 'Click to read' ;
 						myInfo.append(strong, par, pic, link) ;
-						optionFinal = myInfo ;
+						await optionFinal = myInfo ;
 
 				};
-	 }) 
 
-	.catch((error) => {
-				const mistake = document.createTextNode(`Error: ${error.message}`);
-      				placing.innerHTML = mistake ;
-      				console.error(error);
-    });
+
